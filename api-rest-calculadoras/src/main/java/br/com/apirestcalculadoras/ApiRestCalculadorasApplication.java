e929268660f56df159972a2b9c2571c59c5a7acf
William de Souza <william.sza@outlook.com>
2023-06-30 15:29:23 -0300

    Criação do projeto

---
+++ api-rest-calculadoras/src/main/java/br/com/apirestcalculadoras/ApiRestCalculadorasApplication.java
@@ -0,0 +1,13 @@
+package br.com.apirestcalculadoras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCalculadorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCalculadorasApplication.class, args);
+	}
+
+}
---
+++ api-rest-calculadoras/src/test/java/br/com/apirestcalculadoras/ApiRestCalculadorasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apirestcalculadoras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCalculadorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
